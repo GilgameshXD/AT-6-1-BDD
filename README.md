89aff5e8a9dd70f945ec0e351a583ad75e4c385f
Alexander <brolyspowerismaximum@gmail.com>
2024-01-16 23:18:04 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Проверки:
+* ## AppVeyor: [![Build status](https://ci.appveyor.com/api/projects/status/6f81eifwhn0964g0?svg=true)](https://ci.appveyor.com/project/GilgameshXD/at-6-1-bdd)
---
+++ src/test/java/test/MoneyTransferTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import data.DataHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.DashboardPage;
+import page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.DataHelper.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+    DataHelper.CardInfo firstCardInfo;
+    DataHelper.CardInfo secondCardInfo;
+    int firstCardBalance;
+    int secondCardBalance;
+
+    @BeforeEach
+    void setupForTests() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+        firstCardInfo = getFirstCardInfo();
+        secondCardInfo = getSecondCardInfo();
+        firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+    }
+
+    @Test
+    void shouldTransferFromFirstToSecond() {
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceOnFirstCard = firstCardBalance - amount;
+        var expectedBalanceOnSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceOnFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceOnSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceOnFirstCard, actualBalanceOnFirstCard);
+        assertEquals(expectedBalanceOnSecondCard, actualBalanceOnSecondCard);
+    }
+    @Test
+    void shouldTransferFromSecondToFirst() {
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceOnFirstCard = firstCardBalance + amount;
+        var expectedBalanceOnSecondCard = secondCardBalance - amount;
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), secondCardInfo);
+        var actualBalanceOnFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceOnSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceOnFirstCard, actualBalanceOnFirstCard);
+        assertEquals(expectedBalanceOnSecondCard, actualBalanceOnSecondCard);
+    }
+}
